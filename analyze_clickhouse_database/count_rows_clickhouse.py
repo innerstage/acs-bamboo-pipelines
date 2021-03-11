8721b8eeb8da0ff7ae379e2c8633c150fca6e3c2
innerstage <mbarbierif@gmail.com>
2021-03-10 21:19:58 -0300

    show tables

--- analyze_clickhouse_database/count_rows_clickhouse.py
+++ analyze_clickhouse_database/count_rows_clickhouse.py
@@ -5,7 +5,7 @@ from bamboo_lib.helpers import grab_connector, query_to_df
 
 db_connector = grab_connector(__file__, "clickhouse-remote")
 
-query = "select * from acs_ygmt_mortgage_status_by_real_estate_taxes_1 limit 5;"
+query = "show tables"
 
 df = query_to_df(db_connector, query)
 
